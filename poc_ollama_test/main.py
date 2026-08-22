e5c8c7bf6849d8fd0932feeff5e95de7ec6e977b
SpandyX007 <spandanar1234@gmail.com>
2026-08-22 22:03:42 +0530

    feat: add FastAPI application with ollama integration and create .gitignore

--- poc_ollama_test/main.py
+++ poc_ollama_test/main.py
@@ -1,1 +1,8 @@
-from fastapi import Fastapi
+import ollama
+response = ollama.chat(model='qwen:1.8b', messages=[
+  {
+    'role': 'user',
+    'content': 'Hi!',
+  },
+])
+print(response['message']['content'])
